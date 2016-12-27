093d18375564a0468866e03b3b8a7b86355be2b8
soukiab <soukiassianb@gmail.com>
2016-12-27 15:45:11 +0100

    Change urls to reflect backend changes

--- bender/src/App.js
+++ bender/src/App.js
@@ -50,7 +50,7 @@ export default class App extends Component {
 
   fetchData (experimentId) {
     this.setState({algos: [], trials: []})
-    fetch('https://api.rythm.co/v1/dreem/bender/trials_for_experiment/' + experimentId + '/', {
+    fetch('https://api.rythm.co/v1/dreem/bender/experiments/' + experimentId + '/trials/', {
       headers: {
         'Content-type': 'application/json',
         'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI2ZDExNTY0YzczM2U0MDhkYTRiYzVlZWYxNjE5NTMxZiIsImV4cCI6MTQ3MTQ2MDE4NiwicGVybWlzc2lvbnMiOiJoZWFkYmFuZD10ZWFtO25vY3Rpcz1hZG1pbjtkcmVlbWVyPXRlYW07Y3VzdG9tZXI9dGVhbTtkYXRhc2V0PXRlYW07bmlnaHRyZXBvcnQ9dGVhbTtkYXRhdXBsb2FkPWFkbWluO2RhdGFzYW1wbGU9dGVhbTthbGdvcnl0aG09dGVhbTtwcm9kdWN0X3Rlc3Rpbmc9dGVhbSJ9.JRDPQVQGZWvd9C6UMNtG2Q0tDxbMgqSk21r6UI8C38w'
@@ -61,7 +61,7 @@ export default class App extends Component {
       this.setState({trials: json})
     })
 
-    fetch('https://api.rythm.co/v1/dreem/bender/algos_for_experiment/' + experimentId + '/', {
+    fetch('https://api.rythm.co/v1/dreem/bender/experiments/' + experimentId + '/algos/', {
       headers: {
         'Content-type': 'application/json',
         'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI2ZDExNTY0YzczM2U0MDhkYTRiYzVlZWYxNjE5NTMxZiIsImV4cCI6MTQ3MTQ2MDE4NiwicGVybWlzc2lvbnMiOiJoZWFkYmFuZD10ZWFtO25vY3Rpcz1hZG1pbjtkcmVlbWVyPXRlYW07Y3VzdG9tZXI9dGVhbTtkYXRhc2V0PXRlYW07bmlnaHRyZXBvcnQ9dGVhbTtkYXRhdXBsb2FkPWFkbWluO2RhdGFzYW1wbGU9dGVhbTthbGdvcnl0aG09dGVhbTtwcm9kdWN0X3Rlc3Rpbmc9dGVhbSJ9.JRDPQVQGZWvd9C6UMNtG2Q0tDxbMgqSk21r6UI8C38w'
