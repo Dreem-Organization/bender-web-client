1d99a37c9f0eaac8b81ed0cfc10ea1bb0ce762a5
soukiab <soukiassianb@gmail.com>
2017-03-21 18:30:17 +0100

    New urls

--- bender/src/constants/globals.js
+++ bender/src/constants/globals.js
@@ -4,17 +4,5 @@ export const storageKey = {
   user: 'dreem-viewer-user'
 }
 
-export const loginURL = (function () {
-  switch (process.env.DREEM_API) {
-    case 'preprod':
-      return 'https://login.staging.rythm.co/token/'
-    case 'eu':
-    default:
-      return 'https://login.rythm.co/token/'
-  }
-})()
-
-//export const API_BASE_URL = 'https://api.rythm.co/v1/dreem/bender/api/'
-export const BASE_URL = 'http://0.0.0.0:8000'
-export const API_BASE_URL = 'http://0.0.0.0:8000/api'
-// export const signupUrl = `${BASE_URL}registration/`
+export const BASE_URL = 'https://bender-api.rythm.co'
+export const API_BASE_URL = 'https://bender-api.rythm.co/api'
