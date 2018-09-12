358d94541971ea5b7ebec9300698746d8dd02318
Dylan HEIRSTRAETEN <dylan@MacBook-Pro-de-Dylan.local>
2018-09-12 17:19:30 +0200

    Clean the whole project : need to test all functionnalities before prod

--- bender/src/constants/globals.js
+++
@@ -1,8 +0,0 @@
-export const storageKey = {
-    username: 'dreem-viewer-username',
-    token: 'dreem-viewer-token',
-    user: 'dreem-viewer-user'
-};
-
-export const BASE_URL = 'https://bender-api.rythm.co';
-export const API_BASE_URL = 'https://bender-api.rythm.co/api';
