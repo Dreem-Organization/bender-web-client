dbc68e2d29a233f088a6fa887cb42217cf337994
Dylan HEIRSTRAETEN <dylan.heirstraeten@epitech.eu>
2019-01-29 11:29:16 +0100

    api url fix

--- app/utils/globals.js
+++ app/utils/globals.js
@@ -1,3 +1,3 @@
 export const BASE_URL = process.env.API_URL || 'https://bender-api.dreem.com';
 export const API_BASE_URL =
-  process.env.API_URL || 'https://bender-api.dreem.com/api';
+  `${process.env.API_URL}/api` || 'https://bender-api.dreem.com/api';
