33b53d5e274419803792884ca8e19fdd43f97738
Davy Jin <davy@rythm.co>
2019-04-15 19:25:27 +0200

    Fix Label component rendering when receiving object content

--- app/components/Label/index.js
+++ app/components/Label/index.js
@@ -3,11 +3,17 @@ import PropTypes from 'prop-types';
 import { light as theme } from 'themeConfig';
 import StyledLabel from './style';
 
+// FIXME: the content field should strictly be a string,
+// either that or the Label component should integrate
+// a formatting feature. To temporarely fix the errors,
+// let's format objects as JSON
 function Label(props) {
   return (
     <StyledLabel className="label" {...props}>
       {Children.toArray(props.children)}
-      {props.content}
+      {typeof props.content === 'object'
+        ? JSON.stringify(props.content)
+        : props.content}
     </StyledLabel>
   );
 }
@@ -16,7 +22,7 @@ Label.propTypes = {
   theme: PropTypes.object,
   size: PropTypes.string,
   children: PropTypes.node,
-  content: PropTypes.string,
+  content: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
 };
 
 Label.defaultProps = {
